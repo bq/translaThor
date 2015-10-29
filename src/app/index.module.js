a7cc561fd1365e23be0d7d6b4cbc6ea3d6b6772a
LauraDRA <laura.delrio.aviles@gmail.com>
2015-10-29 14:01:52 +0100

    Initial code

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('translaThor', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr']);
+
+})();
